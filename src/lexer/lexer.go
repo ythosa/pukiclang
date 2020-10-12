0abbf182f94fe2fb393547f547621f25c6203d5f
ythosa <vasus714@yandex.ru>
2020-10-12 21:55:37 +0300

    Add tokenizing indentifires

--- src/lexer/lexer.go
+++ src/lexer/lexer.go
@@ -51,6 +51,13 @@ func (l *Lexer) NextToken() token.Token {
 	case 0:
 		tok.Literal = ""
 		tok.Type = token.EOF
+	default:
+		if isLetter(l.ch) {
+			tok.Literal = l.readIdentifier()
+			return tok
+		} else {
+			tok = newToken(token.ILLEGAL, l.ch)
+		}
 	}
 
 	l.readChar()
@@ -63,4 +70,16 @@ func newToken(tokenType token.TokenType, ch byte) token.Token {
 		Type: tokenType,
 		Literal: string(ch),
 	}
+}
+
+func (l *Lexer) readIdentifier() string {
+	position := l.position
+	for isLetter(l.ch) {
+		l.readChar()
+	}
+	return l.input[position:l.position]
+}
+
+func isLetter(ch byte) bool {
+	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
 }
