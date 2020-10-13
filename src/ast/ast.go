da7ec20d36536647c8ec7dcff73a50018dadbc9a
ythosa <vasus714@yandex.ru>
2020-10-13 22:16:19 +0300

    LetStatement struct

--- src/ast/ast.go
+++ src/ast/ast.go
@@ -1,5 +1,7 @@
 package ast
 
+import "github.com/ythosa/pukiclang/src/token"
+
 type Node interface {
 	TokenLiteral() string
 }
@@ -25,3 +27,24 @@ func (p *Program) TokenLiteral() string {
 		return ""
 	}
 }
+
+type LetStatement struct {
+	Token token.Token // the token.LET token
+	Name *Identifier
+	Value Expression
+}
+
+func (ls *LetStatement) statementNode() {}
+
+func (ls *LetStatement) TokenLiteral() string {
+	return ls.Token.Literal
+}
+
+type Identifier struct {
+	Token token.Token // the token.IDENT token
+	Value string
+}
+
+func (i *Identifier) expressionNode() {}
+
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
