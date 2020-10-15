2dea3e8a411d4f1a56c3f076296c61070d22fd51
ythosa <vasus714@yandex.ru>
2020-10-15 22:11:24 +0300

    Parser error handling & updated parser tests

--- src/parser/parser.go
+++ src/parser/parser.go
@@ -1,6 +1,8 @@
 package parser
 
 import (
+	"fmt"
+
 	"github.com/ythosa/pukiclang/src/ast"
 	"github.com/ythosa/pukiclang/src/lexer"
 	"github.com/ythosa/pukiclang/src/token"
@@ -11,16 +13,31 @@ type Parser struct {
 
 	curToken  token.Token
 	peekToken token.Token
+
+	errors []string
 }
 
 func New(l *lexer.Lexer) *Parser {
-	p := &Parser{l: l}
+	p := &Parser{
+		l:      l,
+		errors: []string{},
+	}
 
 	// Read two tokens, so curToken and peekToken are both set
 	p.nextToken()
 	p.nextToken()
 
 	return p
+}
+
+func (p *Parser) Errors() []string {
+	return p.errors
+}
+
+func (p *Parser) peekError(t token.TokenType) {
+	msg := fmt.Sprintf("expected next token to be %s, got %s instead",
+		t, p.peekToken.Type)
+	p.errors = append(p.errors, msg)
 }
 
 func (p *Parser) nextToken() {
--- src/parser/parser_test.go
+++ src/parser/parser_test.go
@@ -18,6 +18,7 @@ let foobar = 228;
 	p := parser.New(l)
 
 	program := p.ParseProgram()
+	checkParserErrors(t, p)
 	if program == nil {
 		t.Fatal("ParseProgram() returned nil")
 	}
@@ -40,6 +41,20 @@ let foobar = 228;
 			return
 		}
 	}
+}
+
+func checkParserErrors(t *testing.T, p *parser.Parser) {
+	errors := p.Errors()
+
+	if len(errors) == 0 {
+		return
+	}
+
+	t.Errorf("parser has %d errors", len(errors))
+	for _, msg := range errors {
+		t.Errorf("parser error: %q", msg)
+	}
+	t.FailNow()
 }
 
 func testLetStatement(t *testing.T, s ast.Statement, name string) bool {
