cbf63b2aba9b6c51541a138e405a7b609cc5f084
ythosa <vasus714@yandex.ru>
2020-10-18 21:29:14 +0300

    Parsing call expressions

--- src/parser/parser.go
+++ src/parser/parser.go
@@ -54,6 +54,7 @@ func New(l *lexer.Lexer) *Parser {
     p.registerInfix(token.GT, p.parseInfixExpression)
     p.registerInfix(token.LTEQ, p.parseInfixExpression)
     p.registerInfix(token.GTEQ, p.parseInfixExpression)
+    p.registerInfix(token.LPAREN, p.parseCallExpression)
 
     // Read two tokens, so curToken and peekToken are both set
     p.nextToken()
@@ -187,6 +188,7 @@ var precedences = map[token.TokenType]int{
     token.MINUS:    SUM,
     token.SLASH:    PRODUCT,
     token.ASTERISK: PRODUCT,
+    token.LPAREN:   CALL,
 }
 
 func (p *Parser) peekPrecedence() int {
@@ -422,4 +424,39 @@ func (p *Parser) parseFunctionParameters() []*ast.Identifier {
     }
 
     return identifiers
+}
+
+func (p *Parser) parseCallExpression(function ast.Expression) ast.Expression {
+    exp := &ast.CallExpression{
+        Token:    p.curToken,
+        Function: function,
+    }
+
+    exp.Arguments = p.parseCallArguments()
+
+    return exp
+}
+
+func (p *Parser) parseCallArguments() []ast.Expression {
+    var args []ast.Expression
+
+    if p.peekTokenIs(token.RPAREN) {
+        p.nextToken()
+        return args
+    }
+
+    p.nextToken()
+    args = append(args, p.parseExpression(LOWEST))
+
+    for p.peekTokenIs(token.COMMA) {
+        p.nextToken()
+        p.nextToken()
+        args = append(args, p.parseExpression(LOWEST))
+    }
+
+    if !p.expectPeek(token.RPAREN) {
+        return nil
+    }
+
+    return args
 }
--- src/parser/parser_test.go
+++ src/parser/parser_test.go
@@ -508,6 +508,18 @@ func TestOperatorPrecedenceParsing(t *testing.T) {
             "!(true == true)",
             "(!(true == true))",
         },
+        {
+            "a + add(b * c) + d",
+            "((a + add((b * c))) + d)",
+        },
+        {
+            "add(a, b, 1, 2 * 3, 4 + 5, add(6, 7 * 8))",
+            "add(a, b, 1, (2 * 3), (4 + 5), add(6, (7 * 8)))",
+        },
+        {
+            "add(a + b + c * d / f + g)",
+            "add((((a + b) + ((c * d) / f)) + g))",
+        },
     }
 
     for _, tt := range tests {
@@ -709,7 +721,7 @@ func TestFunctionParameterParsing(t *testing.T) {
 }
 
 func TestCallExpressionParsing(t *testing.T) {
-    input := "add(1, 2 * 3, 4 * 5);"
+    input := "add(1, 2 * 3, 4 + 5);"
 
     l := lexer.New(input)
     p := parser.New(l)
@@ -744,4 +756,58 @@ func TestCallExpressionParsing(t *testing.T) {
     testLiteralExpression(t, exp.Arguments[0], 1)
     testInfixExpression(t, exp.Arguments[1], 2, "*", 3)
     testInfixExpression(t, exp.Arguments[2], 4, "+", 5)
+}
+
+func TestCallExpressionParameterParsing(t *testing.T) {
+    tests := []struct {
+        input         string
+        expectedIdent string
+        expectedArgs  []string
+    }{
+        {
+            input:         "add();",
+            expectedIdent: "add",
+            expectedArgs:  []string{},
+        },
+        {
+            input:         "add(1);",
+            expectedIdent: "add",
+            expectedArgs:  []string{"1"},
+        },
+        {
+            input:         "add(1, 2 * 3, 4 + 5);",
+            expectedIdent: "add",
+            expectedArgs:  []string{"1", "(2 * 3)", "(4 + 5)"},
+        },
+    }
+
+    for _, tt := range tests {
+        l := lexer.New(tt.input)
+        p := parser.New(l)
+        program := p.ParseProgram()
+        checkParserErrors(t, p)
+
+        stmt := program.Statements[0].(*ast.ExpressionStatement)
+        exp, ok := stmt.Expression.(*ast.CallExpression)
+        if !ok {
+            t.Fatalf("stmt.Expression is not ast.CallExpression. got=%T",
+                stmt.Expression)
+        }
+
+        if !testIdentifier(t, exp.Function, tt.expectedIdent) {
+            return
+        }
+
+        if len(exp.Arguments) != len(tt.expectedArgs) {
+            t.Fatalf("wrong number of arguments. want=%d, got=%d",
+                len(tt.expectedArgs), len(exp.Arguments))
+        }
+
+        for i, arg := range tt.expectedArgs {
+            if exp.Arguments[i].String() != arg {
+                t.Errorf("argument %d wrong. want=%q, got=%q", i,
+                    arg, exp.Arguments[i].String())
+            }
+        }
+    }
 }
