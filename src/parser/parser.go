f9cfe0c05a5c711d368b2fa47916e11534535e95
ythosa <vasus714@yandex.ru>
2020-10-18 10:12:53 +0300

    Parser tracing

--- src/parser/parser.go
+++ src/parser/parser.go
@@ -201,6 +201,8 @@ func (p *Parser) curPrecedence() int {
 }
 
 func (p *Parser) parseExpressionStatement() *ast.ExpressionStatement {
+    //defer untrace(trace("parseExpressionStatement"))
+
     stmt := &ast.ExpressionStatement{Token: p.curToken}
 
     stmt.Expression = p.parseExpression(LOWEST)
@@ -218,6 +220,8 @@ func (p *Parser) noPrefixParseFnError(t token.TokenType) {
 }
 
 func (p *Parser) parseExpression(precedence int) ast.Expression {
+    //defer untrace(trace("parseExpression"))
+
     prefix := p.prefixParseFns[p.curToken.Type]
 
     if prefix == nil {
@@ -241,6 +245,8 @@ func (p *Parser) parseExpression(precedence int) ast.Expression {
 }
 
 func (p *Parser) parsePrefixExpression() ast.Expression {
+    //defer untrace(trace("parsePrefixExpression"))
+
     expression := &ast.PrefixExpression{
         Token:    p.curToken,
         Operator: p.curToken.Literal,
@@ -253,6 +259,8 @@ func (p *Parser) parsePrefixExpression() ast.Expression {
 }
 
 func (p *Parser) parseInfixExpression(left ast.Expression) ast.Expression {
+    //defer untrace(trace("parseInfixExpression"))
+
     expression := &ast.InfixExpression{
         Token:    p.curToken,
         Left:     left,
@@ -274,6 +282,8 @@ func (p *Parser) parseIdentifier() ast.Expression {
 }
 
 func (p *Parser) parseIntegerLiteral() ast.Expression {
+    //defer untrace(trace("parseIntegerLiteral"))
+
     lit := &ast.IntegerLiteral{Token: p.curToken}
 
     value, err := strconv.ParseInt(p.curToken.Literal, 0, 64)
