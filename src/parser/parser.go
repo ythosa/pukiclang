d4d52e504f80df759bed21e77fafe247ee1a82c0
ythosa <vasus714@yandex.ru>
2020-10-18 21:35:28 +0300

    Removed some TODOs & Updated tests

--- src/parser/parser.go
+++ src/parser/parser.go
@@ -146,8 +146,11 @@ func (p *Parser) parseLetStatement() *ast.LetStatement {
         return nil
     }
 
-    // TODO: Remove skipping the expressions until we encounter a semicolon
-    for !p.curTokenIs(token.SEMICOLON) {
+    p.nextToken()
+
+    stmt.Value = p.parseExpression(LOWEST)
+
+    if p.peekTokenIs(token.SEMICOLON) {
         p.nextToken()
     }
 
@@ -159,7 +162,9 @@ func (p *Parser) parseReturnStatement() ast.Statement {
 
     p.nextToken()
 
-    for !p.curTokenIs(token.SEMICOLON) {
+    stmt.ReturnValue = p.parseExpression(LOWEST)
+
+    if p.peekTokenIs(token.SEMICOLON) {
         p.nextToken()
     }
 
--- src/parser/parser_test.go
+++ src/parser/parser_test.go
@@ -10,35 +10,34 @@ import (
 )
 
 func TestLetStatement(t *testing.T) {
-    input := `
-let x = 5;
-let y = 10;
-let mamixLoh = 228;
-`
-    l := lexer.New(input)
-    p := parser.New(l)
-
-    program := p.ParseProgram()
-    checkParserErrors(t, p)
-    if program == nil {
-        t.Fatal("ParseProgram() returned nil")
-    }
-    if len(program.Statements) != 3 {
-        t.Fatalf("program.Statements does not contain 3 statements. got=%d",
-            len(program.Statements))
-    }
-
     tests := []struct {
+        input              string
         expectedIdentifier string
+        expectedValue      interface{}
     }{
-        {"x"},
-        {"y"},
-        {"mamixLoh"},
+        {"let x = 5;", "x", 5},
+        {"let y = true;", "y", true},
+        {"let foobar = y;", "foobar", "y"},
     }
 
-    for i, tt := range tests {
-        stmt := program.Statements[i]
+    for _, tt := range tests {
+        l := lexer.New(tt.input)
+        p := parser.New(l)
+        program := p.ParseProgram()
+        checkParserErrors(t, p)
+
+        if len(program.Statements) != 1 {
+            t.Fatalf("program.Statements does not contain 1 statements. got=%d",
+                len(program.Statements))
+        }
+
+        stmt := program.Statements[0]
         if !testLetStatement(t, stmt, tt.expectedIdentifier) {
+            return
+        }
+
+        val := stmt.(*ast.LetStatement).Value
+        if !testLiteralExpression(t, val, tt.expectedValue) {
             return
         }
     }
@@ -84,31 +83,37 @@ func testLetStatement(t *testing.T, s ast.Statement, name string) bool {
 }
 
 func TestReturnStatement(t *testing.T) {
-    input := `
-return 5;
-return 10;
-return 993322;
-`
-    l := lexer.New(input)
-    p := parser.New(l)
-
-    program := p.ParseProgram()
-    checkParserErrors(t, p)
-
-    if len(program.Statements) != 3 {
-        t.Fatalf("program.Statements does not contain 3 statements. got=%d",
-            len(program.Statements))
+    tests := []struct {
+        input         string
+        expectedValue interface{}
+    }{
+        {"return 5;", 5},
+        {"return true;", true},
+        {"return foobar;", "foobar"},
     }
 
-    for _, stmt := range program.Statements {
+    for _, tt := range tests {
+        l := lexer.New(tt.input)
+        p := parser.New(l)
+        program := p.ParseProgram()
+        checkParserErrors(t, p)
+
+        if len(program.Statements) != 1 {
+            t.Fatalf("program.Statements does not contain 1 statements. got=%d",
+                len(program.Statements))
+        }
+
+        stmt := program.Statements[0]
         returnStmt, ok := stmt.(*ast.ReturnStatement)
         if !ok {
-            t.Errorf("stmt not *ast.returnStatement. got=%T", stmt)
-            continue
+            t.Fatalf("stmt not *ast.returnStatement. got=%T", stmt)
         }
         if returnStmt.TokenLiteral() != "return" {
-            t.Errorf("returnStmt.TokenLiteral not 'return', got %q",
+            t.Fatalf("returnStmt.TokenLiteral not 'return', got %q",
                 returnStmt.TokenLiteral())
+        }
+        if testLiteralExpression(t, returnStmt.ReturnValue, tt.expectedValue) {
+            return
         }
     }
 }
