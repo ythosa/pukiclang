1103c36fca025fb5c697688a7562919891f841d1
ythosa <vasus714@yandex.ru>
2020-10-19 20:52:01 +0300

    Null object

--- src/object/object.go
+++ src/object/object.go
@@ -7,6 +7,7 @@ type ObjectType string
 const (
     INTEGER_OBJ = "INTEGER"
     BOOLEAN_OBJ = "BOOLEAN"
+    NULL_OBJ    = "NULL"
 )
 
 type Object interface {
@@ -36,4 +37,14 @@ func (b *Boolean) Inspect() string {
 
 func (b *Boolean) Type() ObjectType {
     return BOOLEAN_OBJ
+}
+
+type Null struct{}
+
+func (n *Null) Type() ObjectType {
+    return NULL_OBJ
+}
+
+func (n *Null) Inspect() string {
+    return "null"
 }
