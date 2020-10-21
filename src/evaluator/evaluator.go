2b45f022b737c511491e2c0014f05005e623547f
ythosa <vasus714@yandex.ru>
2020-10-21 21:46:49 +0300

    Infix operator with integer expressions evaluating

--- src/evaluator/evaluator.go
+++ src/evaluator/evaluator.go
@@ -32,6 +32,11 @@ func Eval(node ast.Node) object.Object {
     case *ast.PrefixExpression:
         right := Eval(node.Right)
         return evalPrefixExpression(node.Operator, right)
+
+    case *ast.InfixExpression:
+        left := Eval(node.Left)
+        right := Eval(node.Right)
+        return evalInfixExpression(node.Operator, left, right)
     }
 
     return nil
@@ -86,4 +91,35 @@ func evalMinusPrefixOperatorExpression(right object.Object) object.Object {
     value := right.(*object.Integer).Value
 
     return &object.Integer{Value: -value}
+}
+
+func evalInfixExpression(operator string, left object.Object, right object.Object) object.Object {
+    switch {
+    case left.Type() == object.INTEGER_OBJ && right.Type() == object.INTEGER_OBJ:
+        return evalInfixIntegerExpression(operator, left, right)
+    default:
+        return NULL
+    }
+}
+
+func evalInfixIntegerExpression(
+    operator string,
+    left object.Object,
+    right object.Object,
+) object.Object {
+    leftVal := left.(*object.Integer).Value
+    rightVal := right.(*object.Integer).Value
+
+    switch operator {
+    case "+":
+        return &object.Integer{Value: leftVal + rightVal}
+    case "-":
+        return &object.Integer{Value: leftVal - rightVal}
+    case "*":
+        return &object.Integer{Value: leftVal * rightVal}
+    case "/":
+        return &object.Integer{Value: leftVal / rightVal}
+    default:
+        return NULL
+    }
 }
