e18b05648e824d4ea109ddb42176afc30d3294fd
ythosa <vasus714@yandex.ru>
2020-10-22 22:52:04 +0300

    Evaluation return statements

--- src/evaluator/evaluator.go
+++ src/evaluator/evaluator.go
@@ -43,6 +43,10 @@ func Eval(node ast.Node) object.Object {
 
     case *ast.IfExpression:
         return evalIfExpression(node)
+
+    case *ast.ReturnStatement:
+        val := Eval(node.ReturnValue)
+        return &object.ReturnValue{Value: val}
     }
 
     return nil
@@ -53,6 +57,10 @@ func evalStatements(stmts []ast.Statement) object.Object {
 
     for _, statement := range stmts {
         result = Eval(statement)
+
+        if returnValue, ok := result.(*object.ReturnValue); ok {
+            return returnValue.Value
+        }
     }
 
     return result
