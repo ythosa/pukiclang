4770e6448b25d67d3d1acc1fd521e20342f09989
ythosa <vasus714@yandex.ru>
2020-10-22 21:44:44 +0300

    Tests for evaluating if-else expressions

--- src/evaluator/evaluator_test.go
+++ src/evaluator/evaluator_test.go
@@ -96,6 +96,31 @@ func TestBangOperator(t *testing.T) {
     }
 }
 
+func TestIfElseExpressions(t *testing.T) {
+    tests := []struct {
+        input    string
+        expected interface{}
+    }{
+        {"if (true) { 10 }", 10},
+        {"if (false) { 10 }", nil},
+        {"if (1) { 10 }", 10},
+        {"if (1 < 2) { 10 }", 10},
+        {"if (1 > 2) { 10 }", nil},
+        {"if (1 > 2) { 10 } else { 20 }", 20},
+        {"if (1 < 2) { 10 } else { 20 }", 10},
+    }
+
+    for _, tt := range tests {
+        evaluated := testEval(tt.input)
+        integer, ok := tt.expected.(int)
+        if ok {
+            testIntegerObject(t, evaluated, int64(integer))
+        } else {
+            testNullObject(t, evaluated)
+        }
+    }
+}
+
 func testEval(input string) object.Object {
     l := lexer.New(input)
     p := parser.New(l)
@@ -129,6 +154,15 @@ func testBooleanObject(t *testing.T, obj object.Object, expected bool) bool {
     if result.Value != expected {
         t.Errorf("object has wrong value. got=%t, want=%t",
             result.Value, expected)
+    }
+
+    return true
+}
+
+func testNullObject(t *testing.T, obj object.Object) bool {
+    if obj != evaluator.NULL {
+        t.Errorf("object is not Null. got=%T (%+v)", obj, obj)
+        return false
     }
 
     return true
