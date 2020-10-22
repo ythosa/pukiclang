e0cd02dafa49a00d63de615cca696528de52745d
ythosa <vasus714@yandex.ru>
2020-10-22 21:52:09 +0300

    Evaluating if expressions

--- src/evaluator/evaluator.go
+++ src/evaluator/evaluator.go
@@ -37,6 +37,12 @@ func Eval(node ast.Node) object.Object {
         left := Eval(node.Left)
         right := Eval(node.Right)
         return evalInfixExpression(node.Operator, left, right)
+
+    case *ast.BlockStatement:
+        return evalStatements(node.Statements)
+
+    case *ast.IfExpression:
+        return evalIfExpression(node)
     }
 
     return nil
@@ -137,5 +143,30 @@ func evalInfixIntegerExpression(
         return nativeBoolToBooleanObject(leftVal != rightVal)
     default:
         return NULL
+    }
+}
+
+func evalIfExpression(ie *ast.IfExpression) object.Object {
+    condition := Eval(ie.Condition)
+
+    if isTruthy(condition) {
+        return Eval(ie.Consequence)
+    } else if ie.Alternative != nil {
+        return Eval(ie.Alternative)
+    } else {
+        return NULL
+    }
+}
+
+func isTruthy(obj object.Object) bool {
+    switch obj {
+    case NULL:
+        return false
+    case TRUE:
+        return true
+    case FALSE:
+        return false
+    default:
+        return true
     }
 }
