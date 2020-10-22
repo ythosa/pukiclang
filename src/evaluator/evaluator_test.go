c12358340e0d8f0ca6d68ef9f2e0af98b2136b43
ythosa <vasus714@yandex.ru>
2020-10-22 22:47:35 +0300

    Tests for return statements evaluation. return statement object struct

--- src/evaluator/evaluator_test.go
+++ src/evaluator/evaluator_test.go
@@ -121,6 +121,23 @@ func TestIfElseExpressions(t *testing.T) {
     }
 }
 
+func TestReturnStatements(t *testing.T) {
+    tests := []struct {
+        input    string
+        expected int64
+    }{
+        {"return 10;", 10},
+        {"return 10; 9;", 10},
+        {"return 2 * 5; 9;", 10},
+        {"9; return 2 * 5; 9;", 10},
+    }
+
+    for _, tt := range tests {
+        evaluated := testEval(tt.input)
+        testIntegerObject(t, evaluated, tt.expected)
+    }
+}
+
 func testEval(input string) object.Object {
     l := lexer.New(input)
     p := parser.New(l)
--- src/object/object.go
+++ src/object/object.go
@@ -5,9 +5,10 @@ import "fmt"
 type ObjectType string
 
 const (
-    INTEGER_OBJ = "INTEGER"
-    BOOLEAN_OBJ = "BOOLEAN"
-    NULL_OBJ    = "NULL"
+    INTEGER_OBJ      = "INTEGER"
+    BOOLEAN_OBJ      = "BOOLEAN"
+    NULL_OBJ         = "NULL"
+    RETURN_VALUE_OBJ = "RETURN_VALUE"
 )
 
 type Object interface {
@@ -41,10 +42,22 @@ func (b *Boolean) Type() ObjectType {
 
 type Null struct{}
 
+func (n *Null) Inspect() string {
+    return "null"
+}
+
 func (n *Null) Type() ObjectType {
     return NULL_OBJ
 }
 
-func (n *Null) Inspect() string {
-    return "null"
+type ReturnValue struct {
+    Value Object
+}
+
+func (rv *ReturnValue) Inspect() string {
+    return rv.Value.Inspect()
+}
+
+func (rv *ReturnValue) Type() ObjectType {
+    return RETURN_VALUE_OBJ
 }
