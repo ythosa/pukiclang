767cde3c4e716e87e698cee43222e328c02eaada
ythosa <vasus714@yandex.ru>
2020-10-30 07:23:03 +0300

    Update REPL and fix eval

--- src/evaluator/evaluator.go
+++ src/evaluator/evaluator.go
@@ -14,15 +14,15 @@ var (
 )
 
 // Eval evals node of AST tree
-func Eval(node ast.Node) object.Object {
+func Eval(node ast.Node, env *object.Environment) object.Object {
 	switch node := node.(type) {
 	// Statements
 	case *ast.Program:
-		return evalProgram(node)
+		return evalProgram(node, env)
 
 	// Expressions
 	case *ast.ExpressionStatement:
-		return Eval(node.Expression)
+		return Eval(node.Expression, env)
 
 	case *ast.IntegerLiteral:
 		return &object.Integer{
@@ -33,7 +33,7 @@ func Eval(node ast.Node) object.Object {
 		return nativeBoolToBooleanObject(node.Value)
 
 	case *ast.PrefixExpression:
-		right := Eval(node.Right)
+		right := Eval(node.Right, env)
 		if isError(right) {
 			return right
 		}
@@ -41,12 +41,12 @@ func Eval(node ast.Node) object.Object {
 		return evalPrefixExpression(node.Operator, right)
 
 	case *ast.InfixExpression:
-		left := Eval(node.Left)
+		left := Eval(node.Left, env)
 		if isError(left) {
 			return left
 		}
 
-		right := Eval(node.Right)
+		right := Eval(node.Right, env)
 		if isError(right) {
 			return right
 		}
@@ -54,13 +54,13 @@ func Eval(node ast.Node) object.Object {
 		return evalInfixExpression(node.Operator, left, right)
 
 	case *ast.BlockStatement:
-		return evalBlockStatement(node.Statements)
+		return evalBlockStatement(node.Statements, env)
 
 	case *ast.IfExpression:
-		return evalIfExpression(node)
+		return evalIfExpression(node, env)
 
 	case *ast.ReturnStatement:
-		val := Eval(node.ReturnValue)
+		val := Eval(node.ReturnValue, env)
 		if isError(val) {
 			return val
 		}
@@ -68,7 +68,7 @@ func Eval(node ast.Node) object.Object {
 		return &object.ReturnValue{Value: val}
 
 	case *ast.LetStatement:
-		val := Eval(node.Value)
+		val := Eval(node.Value, env)
 		if isError(val) {
 			return val
 		}
@@ -77,11 +77,11 @@ func Eval(node ast.Node) object.Object {
 	return nil
 }
 
-func evalProgram(program *ast.Program) object.Object {
+func evalProgram(program *ast.Program, env *object.Environment) object.Object {
 	var result object.Object
 
 	for _, statement := range program.Statements {
-		result = Eval(statement)
+		result = Eval(statement, env)
 
 		switch result := result.(type) {
 		case *object.ReturnValue:
@@ -94,11 +94,11 @@ func evalProgram(program *ast.Program) object.Object {
 	return result
 }
 
-func evalBlockStatement(stmts []ast.Statement) object.Object {
+func evalBlockStatement(stmts []ast.Statement, env *object.Environment) object.Object {
 	var result object.Object
 
 	for _, statement := range stmts {
-		result = Eval(statement)
+		result = Eval(statement, env)
 
 		if result != nil {
 			rt := result.Type()
@@ -201,16 +201,16 @@ func evalInfixIntegerExpression(
 	}
 }
 
-func evalIfExpression(ie *ast.IfExpression) object.Object {
-	condition := Eval(ie.Condition)
+func evalIfExpression(ie *ast.IfExpression, env *object.Environment) object.Object {
+	condition := Eval(ie.Condition, env)
 	if isError(condition) {
 		return condition
 	}
 
 	if isTruthy(condition) {
-		return Eval(ie.Consequence)
+		return Eval(ie.Consequence, env)
 	} else if ie.Alternative != nil {
-		return Eval(ie.Alternative)
+		return Eval(ie.Alternative, env)
 	} else {
 		return NULL
 	}
--- src/repl/repl.go
+++ src/repl/repl.go
@@ -1,45 +1,49 @@
 package repl
 
 import (
-    "bufio"
-    "fmt"
-    "io"
+	"bufio"
+	"fmt"
+	"io"
 
-    "github.com/ythosa/pukiclang/src/evaluator"
-    "github.com/ythosa/pukiclang/src/lexer"
-    "github.com/ythosa/pukiclang/src/parser"
+	"github.com/ythosa/pukiclang/src/object"
+
+	"github.com/ythosa/pukiclang/src/evaluator"
+	"github.com/ythosa/pukiclang/src/lexer"
+	"github.com/ythosa/pukiclang/src/parser"
 )
 
-const PROMPT = ">> "
+const prompt = ">> "
 
+// Start starts REPL
 func Start(in io.Reader, out io.Writer) {
-    scanner := bufio.NewScanner(in)
+	scanner := bufio.NewScanner(in)
+	env := object.NewEnvironment()
 
-    for {
-        fmt.Print(PROMPT)
-        scanned := scanner.Scan()
-        if !scanned {
-            return
-        }
+	for {
+		fmt.Print(prompt)
+		scanned := scanner.Scan()
+		if !scanned {
+			return
+		}
 
-        line := scanner.Text()
-        l := lexer.New(line)
-        p := parser.New(l)
+		line := scanner.Text()
+		l := lexer.New(line)
+		p := parser.New(l)
 
-        program := p.ParseProgram()
-        if len(p.Errors()) != 0 {
-            printParserErrors(out, p.Errors())
-            continue
-        }
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
+		}
 
-        evaluated := evaluator.Eval(program)
-        _, _ = io.WriteString(out, evaluated.Inspect())
-        _, _ = io.WriteString(out, "\n")
-    }
+		evaluated := evaluator.Eval(program, env)
+		_, _ = io.WriteString(out, evaluated.Inspect())
+		_, _ = io.WriteString(out, "\n")
+	}
 }
 
 func printParserErrors(out io.Writer, errors []string) {
-    for _, msg := range errors {
-        _, _ = io.WriteString(out, "\t"+msg+"\n")
-    }
+	for _, msg := range errors {
+		_, _ = io.WriteString(out, "\t"+msg+"\n")
+	}
 }
