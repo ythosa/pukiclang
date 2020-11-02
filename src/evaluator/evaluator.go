8f39aa29cf49b938ad3da6b20f13e5240529cd20
ythosa <vasus714@yandex.ru>
2020-11-02 14:47:21 +0300

    Array literals evaluation

--- src/evaluator/evaluator.go
+++ src/evaluator/evaluator.go
@@ -106,6 +106,14 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 		}
 
 		return applyFunction(function, args)
+
+	case *ast.ArrayLiteral:
+		elements := evalExpressions(node.Elements, env)
+		if len(elements) == 1 && isError(elements[0]) {
+			return elements[0]
+		}
+
+		return &object.Array{Elements: elements}
 	}
 
 	return nil
