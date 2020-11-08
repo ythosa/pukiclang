c159d5302e82e7f48e294fabd16448e360f54ddf
ythosa <vasus714@yandex.ru>
2020-11-08 21:44:54 +0300

    Hash keys and hash maps types object

--- src/object/object.go
+++ src/object/object.go
@@ -3,6 +3,7 @@ package object
 import (
 	"bytes"
 	"fmt"
+	"hash/fnv"
 	"strings"
 
 	"github.com/ythosa/pukiclang/src/ast"
@@ -217,4 +218,71 @@ func (a *Array) Inspect() string {
 	out.WriteString("]")
 
 	return out.String()
+}
+
+// Hash is type for hash map objects
+type Hash struct {
+	Pairs map[Object]Object
+}
+
+// Type returns type of object
+func (h *Hash) Type() Type {
+	return HashObj
+}
+
+// Inspect returns string representation of object
+func (h *Hash) Inspect() string {
+	var out bytes.Buffer
+
+	pairs := []string{}
+	for k, v := range h.Pairs {
+		pairs = append(pairs, k.Inspect()+":"+v.Inspect())
+	}
+
+	out.WriteString("{")
+	out.WriteString(strings.Join(pairs, ", "))
+	out.WriteString("}")
+
+	return out.String()
+}
+
+// HashKey is type for keys in hash object
+type HashKey struct {
+	Type  Type
+	Value uint64
+}
+
+// HashKey return HashKey object for the current object
+func (b *Boolean) HashKey() HashKey {
+	var value uint64
+
+	if b.Value {
+		value = 1
+	} else {
+		value = 0
+	}
+
+	return HashKey{
+		Type:  b.Type(),
+		Value: value,
+	}
+}
+
+// HashKey return HashKey object for the current object
+func (i *Integer) HashKey() HashKey {
+	return HashKey{
+		Type:  i.Type(),
+		Value: uint64(i.Value),
+	}
+}
+
+// HashKey return HashKey object for the current object
+func (s *String) HashKey() HashKey {
+	h := fnv.New64a()
+	h.Write([]byte(s.Value))
+
+	return HashKey{
+		Type:  s.Type(),
+		Value: h.Sum64(),
+	}
 }
--- src/object/object_names.go
+++ src/object/object_names.go
@@ -11,4 +11,5 @@ const (
 	FunctionObj    = "FUNCTION"
 	BuiltInObj     = "BUILTIN"
 	ArrayObj       = "ARRAY"
+	HashObj        = "HASH"
 )
